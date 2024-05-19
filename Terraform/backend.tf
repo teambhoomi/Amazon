15a877c90a0d4be872aa581a13c4a67837358d46
Varunkumar2698 <127874669+Varunkumar2698@users.noreply.github.com>
2024-05-19 14:13:50 +0530

    Update backend.tf
    removed remote backend code

--- Terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    key = "Jenkins"
-    resource_group_name = "value"
-    storage_account_name = "value"
-    container_name = "value"
-    access_key = "value"
-  }
-}
