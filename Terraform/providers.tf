3168ed3d0a07e3a2d919e9a21cf3574ab6cd8141
teambhoomi <bhoomiteam25@gmail.com>
2024-05-20 16:56:44 +0530

    Update providers.tf

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -18,8 +18,8 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
-  client_id       = var.tf_var_client_id
-  subscription_id = var.tf_var_subscription_id
-  client_secret   = var.tf_var_client_secret
-  tenant_id       = var.tf_var_tenant_id
+  //client_id       = var.tf_var_client_id
+  //subscription_id = var.tf_var_subscription_id
+  //client_secret   = var.tf_var_client_secret
+  //tenant_id       = var.tf_var_tenant_id
 }
