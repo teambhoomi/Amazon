7214faf615d175a100e2b7b4adec0b29be8b0bfa
teambhoomi <bhoomiteam25@gmail.com>
2024-05-20 16:31:55 +0530

    Create variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "tf_var_client_id" {
+  description = "Azure Service Principal Client ID"
+}
+
+variable "tf_var_subscription_id" {
+  description = "Azure Subscription ID"
+}
+
+variable "tf_var_client_secret" {
+  description = "Azure Service Principal Client Secret"
+}
+variables "tf_var_tenant_id" {
+  description = "tenant id of azure"
+}
